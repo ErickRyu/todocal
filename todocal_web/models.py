461a34a3a23e736be79081b682f7b469ae755b1f
ErickRyu <rkggmdii@gmail.com>
2016-08-16 17:22:16 +0900

    추가. Scheduel 모델을 추가하다.

--- todocal_web/models.py
+++ todocal_web/models.py
@@ -8,3 +8,15 @@ class User(models.Model):
 
   def __str__(self):
     return self.name
+
+class Schedule(models.Model):
+  start_time = models.DateField()
+  end_time = models.DateField()
+  name = models.CharField(max_length=50)
+  memo = models.TextField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  repeating = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
+    

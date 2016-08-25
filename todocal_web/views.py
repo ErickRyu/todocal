e3cfc5907ac89310fe97718f15828a064514f400
ErickRyu <rkggmdii@gmail.com>
2016-08-25 17:02:16 +0900

    추가: 스케쥴들을 보여주는 schedule_list 를 추가하다.

--- todocal_web/views.py
+++ todocal_web/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import User, Schedule
+from django.utils import timezone
 
 # Create your views here.
+def schedule_list(request):
+	schedules = Schedule.objects.order_by('start_time')
+	
+
+	return render(request, 'todocal_web/schedule_list.html', {'schedules' : schedules})	

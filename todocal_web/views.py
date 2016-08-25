a0d9e456ab82f9f514ccf0bce3231c01f8ed33f6
ErickRyu <rkggmdii@gmail.com>
2016-08-25 18:55:40 +0900

    추가: schedule을 추가하는 view를 추가하다.

--- todocal_web/views.py
+++ todocal_web/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import User, Schedule
 from django.utils import timezone
+from .forms import ScheduleForm
 
 # Create your views here.
 def schedule_list(request):
@@ -13,3 +14,16 @@ def schedule_detail(request, pk):
 	schedule = get_object_or_404(Schedule, pk=pk)
 
 	return render(request, 'todocal_web/schedule_detail.html', {'schedule': schedule})
+
+
+
+def schedule_new(request):
+	if request.method == "POST":
+		form = ScheduleForm(request.POST)
+		if form.is_valid():
+			schedule = form.save(commit=False)
+			schedule.save()
+			return redirect('schedule_detail', pk=schedule.pk)
+	else:
+		form = ScheduleForm()
+	return render(request, 'todocal_web/schedule_edit.html', {'form' : form})

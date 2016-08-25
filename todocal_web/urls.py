e8ab317e61a73f1379e93d0d1dade99912751990
ErickRyu <rkggmdii@gmail.com>
2016-08-25 17:52:22 +0900

    추가: schedule_detail페이지로 연결하는 패턴을 추가하다.

--- todocal_web/urls.py
+++ todocal_web/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.schedule_list, name='schedule_list'),
-	
+	url(r'^schedule/(?P<pk>[0-9]+)/$', views.schedule_detail, name='schedule_detail'),
 	
 ]
